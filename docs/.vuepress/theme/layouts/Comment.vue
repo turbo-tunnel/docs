0efccab4ba6e66e565c583f6d8712663d1c0a3a7
drunkdream <drunkdream@qq.com>
2022-07-12 01:12:19 +0800

    Fix cannot reload when location changed

--- docs/.vuepress/theme/layouts/Comment.vue
+++ docs/.vuepress/theme/layouts/Comment.vue
@@ -11,17 +11,34 @@
   export default {
     name: 'comment',
     mounted() {
-      const commentConfig = {
-        clientID: 'Iv1.bd9184303aaf4a57',
-        clientSecret: 'd88fb04084e97ffaf2d4f23dc01b69105f932318',
-        repo: 'docs',
-        owner: 'turbo-tunnel',
-        admin: ['drunkdream'],
-        id: location.pathname,
-        proxy: 'https://corsproxy.io/?https://github.com/login/oauth/access_token'
-      };
-      const gitalk = new Gitalk(commentConfig);
-      gitalk.render('gitalk-container');
+      this.loadGitalk();
+
+      this.$router.afterEach((to, from) => {
+        this.$nextTick(function() {
+          //页面Dom加载完成后执行
+          if (to.path != from.path) {
+            this.loadGitalk();
+          }
+        });
+      });
+    },
+    methods: {
+      loadGitalk() {
+        console.log(`load gitalk in ${location.href}`);
+        const commentConfig = {
+          clientID: 'Iv1.bd9184303aaf4a57',
+          clientSecret: 'd88fb04084e97ffaf2d4f23dc01b69105f932318',
+          repo: 'docs',
+          owner: 'turbo-tunnel',
+          admin: ['drunkdream'],
+          id: location.pathname,
+          proxy: 'https://corsproxy.io/?https://github.com/login/oauth/access_token'
+        };
+        const container = document.getElementById('gitalk-container');
+        container.innerHTML = '';
+        const gitalk = new Gitalk(commentConfig);
+        gitalk.render('gitalk-container');
+      }
     }
   };
 </script>
